e17a9800a4dded6bc188923c7d11aa7d9da100d3
USER <user@USERnoMacBook-Air.local>
2025-08-30 22:03:54 +0900

    feat: 87appブランチを花屋向け店舗管理システムとして完全に再構築 - 花屋向けの全機能ページをルーティングに追加 - 顧客管理、商品管理、店舗登録、決済システムなど - 適切な認証と保護されたルートの設定 - 87appと87app-customerの明確な分離

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,20 @@ import { SimpleAuthProvider } from './contexts/SimpleAuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 
-// 顧客アプリのページコンポーネント
+// 花屋向け店舗管理システムのページコンポーネント
+const Home = React.lazy(() => import('./pages/Home').then(module => ({ default: module.Home })));
 const SimpleLoginForm = React.lazy(() => import('./components/auth/SimpleLoginForm').then(module => ({ default: module.SimpleLoginForm })));
 const SignUpForm = React.lazy(() => import('./components/auth/SignUpForm').then(module => ({ default: module.SignUpForm })));
-const MenuPage = React.lazy(() => import('./pages/MenuPage').then(module => ({ default: module.MenuPage })));
+const SimpleMenuScreen = React.lazy(() => import('./pages/SimpleMenuScreen').then(module => ({ default: module.SimpleMenuScreen })));
 const FloristMap = React.lazy(() => import('./pages/FloristMap').then(module => ({ default: module.FloristMap })));
+const FlowerLessonMap = React.lazy(() => import('./pages/FlowerLessonMap'));
+const CustomerManagement = React.lazy(() => import('./pages/CustomerManagement').then(module => ({ default: module.CustomerManagement })));
+const ProductManagement = React.lazy(() => import('./pages/ProductManagement'));
+const StoreRegistration = React.lazy(() => import('./pages/StoreRegistration').then(module => ({ default: module.StoreRegistration })));
+const CheckoutScreen = React.lazy(() => import('./pages/CheckoutScreen'));
+const LessonSchoolManagement = React.lazy(() => import('./pages/LessonSchoolManagement'));
+const LessonScheduleManagement = React.lazy(() => import('./pages/LessonScheduleManagement'));
+const PopularityRankings = React.lazy(() => import('./pages/PopularityRankings'));
 const QRCodePage = React.lazy(() => import('./pages/QRCodePage').then(module => ({ default: module.QRCodePage })));
 const PaymentPage = React.lazy(() => import('./pages/PaymentPage').then(module => ({ default: module.PaymentPage })));
 
@@ -37,19 +46,70 @@ function App() {
                   <Suspense fallback={<PageLoader />}>
                     <Routes>
                       {/* 認証ページ */}
-                      <Route path="/login" element={<SimpleLoginForm />} />
+                      <Route path="/simple-login" element={<SimpleLoginForm />} />
                       <Route path="/signup" element={<SignUpForm />} />
                       
-                      {/* 保護されたルート */}
-                      <Route path="/" element={
+                      {/* ホームページ */}
+                      <Route path="/" element={<Home />} />
+                      
+                      {/* 保護されたルート - 花屋向け機能 */}
+                      <Route path="/menu" element={
                         <ProtectedRoute>
-                          <MenuPage />
+                          <SimpleMenuScreen />
                         </ProtectedRoute>
                       } />
                       
                       <Route path="/florist-map" element={
                         <ProtectedRoute>
                           <FloristMap />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/flower-lesson-map" element={
+                        <ProtectedRoute>
+                          <FlowerLessonMap />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/customer-management" element={
+                        <ProtectedRoute>
+                          <CustomerManagement />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/product-management" element={
+                        <ProtectedRoute>
+                          <ProductManagement />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/store-registration" element={
+                        <ProtectedRoute>
+                          <StoreRegistration />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/checkout" element={
+                        <ProtectedRoute>
+                          <CheckoutScreen />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/lesson-school-management" element={
+                        <ProtectedRoute>
+                          <LessonSchoolManagement />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/lesson-schedule-management" element={
+                        <ProtectedRoute>
+                          <LessonScheduleManagement />
+                        </ProtectedRoute>
+                      } />
+                      
+                      <Route path="/popularity-rankings" element={
+                        <ProtectedRoute>
+                          <PopularityRankings />
                         </ProtectedRoute>
                       } />
                       
